2f1849233a12f63eb184378f6bb6b3a934c67aeb
Quint Guvernator <quint@guvernator.net>
2020-07-03 15:53:41 +0200

    sprites.asm:SpriteSet__a: update a sprite in OAM based on its current RAM SMT state

--- src/sprites.asm
+++ src/sprites.asm
@@ -1,3 +1,57 @@
+; Set the OAM tile index and attributes from its (RAM) SMT state.
+; arg a: sprite index in RAM SMT
+SpriteSet__a:
+	ld	b,a			; b <- sprite index
+	ld	hl,_OAMRAM		; hl <- &OAMRAM[sprite_index].tile_id
+	sla	a
+	sla	a
+	inc	a
+	inc	a
+	addhla
+	push	hl			; save &OAMRAM[sprite_index]
+	ld	hl,SMT_RAM		; hl <- &SMT[sprite_index]
+	ld	a,b
+if SMT_RAM_BYTES == 8
+rept 3
+	sla	a
+endr
+else
+fail "optimization for `a *= SMT_RAM_BYTES` via rotation in SpriteSet__a in src/sprites.asm no longer applies!"
+endc
+rept 3					; hl <- &SMT[sprite_index].anim_counter
+	inc	a
+endr
+	addhla
+	; push	hl
+	; pop	hl
+	ld	a,[hl]			; d <- SMT[sprite_index].anim_counter
+	ld	d,a
+	inc	hl			; hl <- &SMT[sprite_index].anim_table_address
+	ld	a,[hl+]			; bc <- &anim_table
+	ld	c,a
+	ld	a,[hl+]
+	ld	b,a
+	;				; hl <- &SMT[sprite_index].flag_table_address
+	ld	a,d			; bc <- &anim_table[anim_counter]
+	addbca
+	ld	a,[bc]			; e <- anim_table[anim_counter]
+	ld	e,a
+	ld	a,[hl+]			; bc <- &flag_table
+	ld	b,a
+	ld	a,[hl]
+	ld	c,a
+	ld	a,d			; bc <- &flag_table[anim_counter]
+	addbca
+	ld	a,[bc]			; d <- flag_table[anim_counter]
+	ld	d,a
+	pop	hl			; hl <- &OAMRAM[sprite_index].tile_id
+	ld	a,e			; OAMRAM[sprite_index].tile_id <- anim_table[anim_counter]
+	ld	[hl+],a
+	;				; hl <- &OAMRAM[sprite_index].attrs
+	ld	a,d			; OAMRAM[sprite_index].attrs <- flag_table[anim_counter]
+	ld	[hl],a
+	ret
+
 AnimateSprites:
 	ld	a,SPRITE_NUM
 .loop
@@ -53,6 +107,7 @@ PositionUpdate__a:
 	ret
 
 ; arg a: sprite index in RAM SMT
+; FIXME don't update OAM, just call SpriteSet__a after updating the SMT
 AdvanceAnimation__a:
 	ld	c,a		; c <- i
 	ld	d,a		; d <- i
